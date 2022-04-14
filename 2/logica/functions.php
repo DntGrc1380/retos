9c8085863ef494f7d38a6becc7db058f19f59cff
DntGrc1380 <103540769+DntGrc1380@users.noreply.github.com>
2022-04-13 20:06:15 -0500

    Problema 2, 2.1
    Agregué la interfaz para poder seleccionar el archivo a procesar.
    Cambio la salida del archivo de 2 líneas a una sola línea separada por espacio.
    Le doy un poco de orden.

---
+++ 2/logica/functions.php
@@ -0,0 +1,40 @@
+<?php
+    function procesaArchivo($file){
+        $data = file('../archivos/entrada.txt', FILE_IGNORE_NEW_LINES);
+        $count = 0;
+        $arrayMaxWinner = array(0,0);
+        $arrayRoundWinner = array(0,0);
+        //$playerRoundWinner = 0;
+        foreach($data as $d){
+            $count ++;
+            $dif = 0;
+            if($count > 1){
+                //$arrayRound['player1'] = $d[0];
+                //$arrayRound['player2'] = $d[1];
+                $r = explode(" ",$d);
+                if($r[0] > $r[1]){
+                    $dif = $r[0] - $r[1];
+                    $arrayRoundWinner[0] = 1;
+                    $arrayRoundWinner[1] = $dif;
+                }else{
+                    $dif = $r[1] - $r[0];
+                    $arrayRoundWinner[0] = 2;
+                    $arrayRoundWinner[1] = $dif;
+                }
+                echo 'El ganador de la ronda '. $count . ' es: J' . $arrayRoundWinner[0] . ' con: ' . $dif . ' puntos </br>';
+                if ($arrayRoundWinner[1] > $arrayMaxWinner[1]){
+                    $arrayMaxWinner[0] = $arrayRoundWinner[0];
+                    $arrayMaxWinner[1] = $dif;
+                }
+                echo 'El ganador maximo es: J' . $arrayMaxWinner[0] . ' con: ' . $arrayMaxWinner[1] . ' puntos' . "</br></br>";
+            }
+        }
+        generaArchivo('salida.txt',$arrayMaxWinner);
+    }
+    function generaArchivo($nombreArchivo,$winner){
+        $archivo = fopen('../archivos/'.$nombreArchivo,'w');
+        fwrite($archivo,$winner[0] . " " . $winner[1]);
+        fclose($archivo);
+        echo "<b>Archivo generado con exito<b>";
+    }
+?>
---
+++ 2/logica/upload.php
@@ -0,0 +1,46 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Procesando el archivo enviado</title>
+<style type="text/css">
+*{ font-family:Segoe, "Segoe UI", "DejaVu Sans", "Trebuchet MS", Verdana, sans-serif}
+.main{ margin:auto; border:1px solid #7C7A7A; width:50%; text-align:left; padding:30px; background:#85c587}
+input[type=submit]{ background:#6ca16e; width:100%;
+    padding:5px 15px; 
+    background:#ccc; 
+    cursor:pointer;
+	font-size:16px;
+   
+}
+table td{ padding:5px;}
+</style>
+</head>
+
+<body bgcolor="#bed7c0">
+<div class="main">
+<h1>Archivo procesado:</h1>
+<?php
+    include '../logica/functions.php';
+    $directorio = '../archivos/';
+    $subir_archivo = $directorio.basename($_FILES['subir_archivo']['name']);
+    $descarga = $directorio. 'salida.txt';
+    echo "<div>";
+    if (move_uploaded_file($_FILES['subir_archivo']['tmp_name'], $subir_archivo)) {
+        echo "El archivo es válido y se cargó correctamente.<br><br>";
+        procesaArchivo($subir_archivo);
+        echo "<br>";
+	    echo"<a href='".$descarga."' target='_blank'>Descargar archivo</a>";
+    } else {
+       echo "La subida ha fallado";
+    }
+    echo "</div>";
+?>
+<br>
+<div style="border:1px solid #000000; text-transform:uppercase">  
+<h3 align="center"><div align="center"><a href="../index.html">Volver </a></div></h3></div>
+
+ 
+</div>
+	</body>
+</html>
